28bc9f102ab270470d1fff9b5d24948f6f60829b
Alberto González <alberto.gonzalez@distillery.com>
2022-03-18 14:55:45 -0600

    The request to the geolocation API was implemented.

--- app.js
+++ app.js
@@ -9,3 +9,11 @@ request({ url, json: true }, (error, response) => {
   console.log("Tempeture: ", temperature, "Feels: ", feelslike);
   console.log("Descriptions: ", weather_descriptions[0]);
 });
+
+const urlGEO = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${process.env.ACCESS_TOKEN}`;
+request({ url: urlGEO, json: true }, (error, response) => {
+  const centerArray = response.body.features[0].center;
+  const longitude = centerArray[0];
+  const latitude = centerArray[1];
+  console.log("Longitude: ", longitude, "Latitude: ", latitude);
+});
